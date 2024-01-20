a99d6f6695dc636d315a3f7eacf7d76bbc3aa2b5
iamdeepakdass <iamdeepakdass@gmail.com>
2024-01-20 21:43:45 +0530

    sample testing

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ function App() {
       <h1 className="text-4xl w-1/2 mx-auto text-center  text-cyan-400  border-[2px] border-cyan-200 hover:text-6xl">
         hello
       </h1>
+      <h2>Tried some code pushing</h2>
     </>
   );
 }
