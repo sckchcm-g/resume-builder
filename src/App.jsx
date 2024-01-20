e60881a0d722bf844efe70f298721cb44f6849cc
priyanshu tiwari <priyanshu108tiwari@gmail.com>
2024-01-20 15:23:39 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl w-1/2 mx-auto text-center  text-cyan-400  border-[2px] border-cyan-200 hover:text-6xl">
+        hello
+      </h1>
+    </>
+  );
+}
+
+export default App;
