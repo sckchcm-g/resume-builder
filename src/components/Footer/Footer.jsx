a3e7debf1fcf170e6feab3180a74172542776fa6
iamdeepakdass <iamdeepakdass@gmail.com>
2024-01-21 21:52:11 +0530

    Home page and router created

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="bg-slate-500 w-full shadow-lg bottom-0">
+      <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-center">
+        <span className="text-sm text-center text-white">
+          &#169; 2024{" "}
+          <a href="https://google.com/" className="hover:underline">
+            Inspiration &trade;
+          </a>
+          &nbsp;All rights reserved. Contact Us
+        </span>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import Home from "./Components/Home/Home.jsx";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Forms from "./Components/Forms.jsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Home />} />
+      <Route path="form" element={<Forms />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
